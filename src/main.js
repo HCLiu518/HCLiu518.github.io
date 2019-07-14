44e7b010c97661b89c77ba9ab2ee582bbd553e65
HCLiu <cd5451935@gapp.nthu.edu.tw>
2019-07-14 11:40:37 +0800

    add slides in banner

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction();};
+window.onload = function() {bgSlide()};
+
+var el = document.getElementById("banner");
+var flag = 1;
 
 function scrollFunction() {
-    var el = document.getElementById("banner");
     var elNav = document.getElementById("nav");
     var heightNav = elNav.getBoundingClientRect().height;
     var height = el.getBoundingClientRect().height - heightNav;
@@ -16,4 +19,17 @@ function scrollFunction() {
 
 function paperClick(url) {
     window.open(url, "new-tab");
+}
+
+function bgSlide() {
+    setTimeout(function(){
+        if(flag){
+            el.style.backgroundImage = "url('./images/banner-2.jpg')";
+            flag = 0;
+        }else{
+            el.style.backgroundImage = "url('./images/banner-1.jpg')";
+            flag = 1;
+        }
+        bgSlide();
+    }, 10000);
 }
